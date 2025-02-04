360e31817f15c5ad40e3577c1837ca005f8caebf
Saikiran <sai@50gramx.io>
2025-02-04 14:47:05 +0530

    first commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testAppHasAGreeting() {
+        assertTrue(true, "Hello, World!");
+    }
+}
